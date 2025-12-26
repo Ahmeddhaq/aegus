d272d9dd7eedc1a4bbc15a6fda7975722c62874d
sanzo <sanzofr62@gmail.com>
2025-12-26 23:38:36 +0530

    added about page

---
+++ about/script.js
@@ -0,0 +1,53 @@
+const lenis = new Lenis();
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+const menu = document.querySelector(".header .menu");
+let isOpen = false; // track state
+
+menu.addEventListener("click", () => {
+    if (!isOpen) {
+        // OPEN (turn into X)
+        gsap.to(".header .menu hr:nth-of-type(1)", {
+            rotate: 45,
+            y: 6,
+            duration: 0.3,
+            transformOrigin: "center"
+        });
+
+        gsap.to(".header .menu hr:nth-of-type(2)", {
+            rotate: -45,
+            y: -6,
+            duration: 0.3,
+            transformOrigin: "center"
+        });
+
+        gsap.to(".menu-overlay", {
+            left: "-30%",
+            ease: "power3.inOut",
+            duration: 1,
+        })
+    } else {
+        // CLOSE (reset to hamburger)
+        gsap.to(".header .menu hr:nth-of-type(1)", {
+            rotate: 0,
+            y: 0,
+            duration: 0.3
+        });
+
+        gsap.to(".header .menu hr:nth-of-type(2)", {
+            rotate: 0,
+            y: 0,
+            duration: 0.3
+        });
+        gsap.to(".menu-overlay", {
+            left: "-100%",
+            ease: "power3.inOut",
+            duration: 1,
+        })
+    }
+
+    isOpen = !isOpen;
+});
